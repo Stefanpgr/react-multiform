59b2ffb300fa7cc92ca107d670730318e3868ca2
kwabang01 <kwabang01@gmail.com>
2020-04-29 23:21:01 +0100

    more ui changes

--- src/view/ApplicationPage/forms/Form4.jsx
+++ src/view/ApplicationPage/forms/Form4.jsx
@@ -1,90 +1,105 @@
 import React, { useEffect, useState } from 'react';
 import { Form, Input, InputNumber, Select, Radio } from 'antd';
 import { Col, Row, Container } from 'react-bootstrap';
-import {useDispatch, connect} from 'react-redux'
-import {BottomNav }from '../BottomNav'
+import { useDispatch, connect } from 'react-redux';
+import { BottomNav } from '../BottomNav';
 
 import styled from 'styled-components';
 const StyledSwitch = styled.button`
-		width: 80px;
-		height: 79px;
-		background: #e7f7ff 0% 0% no-repeat padding-box;
-		border: 1px solid #5b55ff;
-		border-radius: 5px;
-		opacity: 1;
-	`;
+	width: 80px;
+	height: 79px;
+	background: #e7f7ff 0% 0% no-repeat padding-box;
+	border: 1px solid #5b55ff;
+	border-radius: 5px;
+	opacity: 1;
+`;
 
-	const Box = styled.div`
-top: 920px;
-left: 650px;
-width: 546px;
-height: 320px;
-background: #EDF3F8 0% 0% no-repeat padding-box;
-border: 1px solid #51A4FB;
-border-radius: 15px;
-opacity: 1;
-`
+const Box = styled.div`
+	top: 920px;
+	left: 650px;
+	width: 546px;
+	height: 320px;
+	background: ${(props) => (props.stat === "I'm still searching" ? '#FFFF' : '#edf3f8 0% 0% no-repeat padding-box')};
+
+	border: ${(props) => (props.stat === "I'm still searching" ? '1px solid #FF6C6C' : '1px solid #51a4fb')};
+	border-radius: 15px;
+	opacity: 1;
+`;
 const StyledRadio = styled.button`
-background: ${(props) => (props.switch === props.stat ? '#e7f7ff' : 0)};
+	background: ${(props) => (props.switch === props.stat ? '#e7f7ff' : 0)};
 
-border: ${(props) => (props.switch === props.stat ? '1px solid #5b55ff' : 0)};
+	border: ${(props) => (props.switch === props.stat ? '1px solid #5b55ff' : 0)};
 
-border-radius: 5px;
-font-family: Rubik, 'sans-serif';
-color: #878787;
-opacity: 1;
-padding: 9px 0px 8px 15px;
+	border-radius: 5px;
+	font-family: Rubik, 'sans-serif';
+	color: #878787;
+	opacity: 1;
+	padding: 9px 0px 8px 15px;
 `;
 const layout = {
 	labelCol: { span: 12 },
 	wrapperCol: { span: 16 }
 };
 
-const Form4 = ({ next,prev, payment, rent}) => {
+const Form4 = ({ next, prev, payment, rent }) => {
 	// console.log(props);
 	const [ accStat, setAcc ] = useState(1);
 	const [ rc, setRc ] = useState('Landlord');
-	const dispatch = useDispatch()
+	const dispatch = useDispatch();
 	const [ form ] = Form.useForm();
 	const validateMessages = {
 		required: 'This field is required!',
 		types: {
 			email: 'Not a validate email!',
 			number: 'Not a validate number!'
-		},
+		}
 		// number: {
 		// 	range: `Must be between ${min} and ${max}`
 		// }
 	};
 
-	useEffect(() => {
-	
-		// const data = JSON.parse(localStorage.getItem('rent'));
-		// const test = JSON.parse(localStorage.getItem('payment'))
-		if(payment){
-			form.setFieldsValue({	acct_stat_show: payment.acct_stat})
-			setAcc(payment.acct_stat)
-		}
-		
-		if (rent) {
-			form.setFieldsValue({
-				acct_stat_show:payment.acct_stat,
-				home_address: rent.home_address,
-				last_rent_amount: rent.last_rent_amount,
-				duration: rent.duration,
-				rent_collector: rent.rent_collector,
-				pay_type: rent.pay_type
-			});
-		}
-	}, [form]);
+	useEffect(
+		() => {
+			// const data = JSON.parse(localStorage.getItem('rent'));
+			// const test = JSON.parse(localStorage.getItem('payment'))
+			if (payment) {
+				form.setFieldsValue({ acct_stat_show: payment.acct_stat });
+				setAcc(payment.acct_stat);
+			}
+
+			if (rent) {
+				form.setFieldsValue({
+					acct_stat_show: payment.acct_stat,
+					home_address: rent.home_address,
+					last_rent_amount: rent.last_rent_amount,
+					duration: rent.duration,
+					rent_collector: rent.rent_collector,
+					pay_type: rent.pay_type
+				});
+			}
+		},
+		[ form ]
+	);
+
+	const Reject = () => {
+		return (
+			<div>
+				<div className="reject-stat-child">!</div>
+			</div>
+		);
+	};
+
+	const ActStatus = () => {
+		return <div />;
+	};
 
 	const onFinish = (values) => {
 		console.log('Received values of form: ', values);
 		dispatch({
 			type: 'ADD_APPL',
-			data:{rent: {...values}, page: 3}
-		})
-		next()
+			data: { rent: { ...values }, page: 3 }
+		});
+		next();
 		// message.success("Processing complete!")
 	};
 
@@ -98,26 +113,41 @@ const Form4 = ({ next,prev, payment, rent}) => {
 			validateMessages={validateMessages}
 			layout="vertical"
 		>
-		
-					<Form.Item
-						name={[ 'home_address' ]}
-						label="What's your current home address?"
-						rules={[ { required: true } ]}
-					>
-						<Input />
-					</Form.Item>
+			<Form.Item
+				name={[ 'home_address' ]}
+				label="What's your current home address?"
+				rules={[ { required: true } ]}
+			>
+				<Input />
+			</Form.Item>
 
-
-					<Form.Item label="How long have you lived here?" name={[ 'duration' ]}>
-          <Radio.Group  buttonStyle="solid">
-            <Radio.Button value={1}><p><strong>1</strong></p><p>Year</p></Radio.Button>
-            <Radio.Button value={2}><p>2</p><p>Years</p> </Radio.Button>
-            <Radio.Button value={3}><p>3</p><p>Years</p>  </Radio.Button>
-			<Radio.Button value={4}><p>4</p><p>Years</p> </Radio.Button>
-            <Radio.Button value={5}><p>5</p><p>Years</p>  </Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-		{/* <Form.Item
+			<Form.Item label="How long have you lived here?" name={[ 'duration' ]}>
+				<Radio.Group buttonStyle="solid">
+					<Radio.Button value={1}>
+						<p>
+							<strong>1</strong>
+						</p>
+						<p>Year</p>
+					</Radio.Button>
+					<Radio.Button value={2}>
+						<p>2</p>
+						<p>Years</p>{' '}
+					</Radio.Button>
+					<Radio.Button value={3}>
+						<p>3</p>
+						<p>Years</p>{' '}
+					</Radio.Button>
+					<Radio.Button value={4}>
+						<p>4</p>
+						<p>Years</p>{' '}
+					</Radio.Button>
+					<Radio.Button value={5}>
+						<p>5</p>
+						<p>Years</p>{' '}
+					</Radio.Button>
+				</Radio.Group>
+			</Form.Item>
+			{/* <Form.Item
 						name={[ 'acc_status_show' ]}
 						label="What's your accomodation status?"
 						rules={[ { required: true } ]}
@@ -131,7 +161,7 @@ const Form4 = ({ next,prev, payment, rent}) => {
 			</Select> 
 		
 					</Form.Item>	<a><span className='ml-3'><small onClick={()=> next(0)}>Change?</small></span></a> */}
-		{/* <Form.Item  label="What's your accomodation status?" name={[ 'acc_stat_show' ]}  >
+			{/* <Form.Item  label="What's your accomodation status?" name={[ 'acc_stat_show' ]}  >
 				<Select size='large'
 				//  value={accStat} 
 				value='hhhh'
@@ -146,83 +176,65 @@ const Form4 = ({ next,prev, payment, rent}) => {
 				</Select>
 			</Form.Item> */}
 
-
-
-
-			<Form.Item name={[ 'acc_stat_show' ]} label="What's your accomodation status?"  >
-				<Select size='large' value={accStat}  style={{width: '65%'}} placeholder={accStat} disabled>
-					
-						<Select.Option  value='card'>
-						Card
-						</Select.Option>
-						<Select.Option  value='bank'>
-						Bank Transfer
-						</Select.Option>
-					
-				</Select> 
+			<Form.Item name={[ 'acc_stat_show' ]} label="What's your accomodation status?">
+				<Select size="large" value={accStat} style={{ width: '65%' }} placeholder={accStat} disabled>
+					<Select.Option value="card">Card</Select.Option>
+					<Select.Option value="bank">Bank Transfer</Select.Option>
+				</Select>
 			</Form.Item>
 
-
-
-			
-			<a><span className='ml-3'><small onClick={()=> next(0)}>Change?</small></span></a>
-<Box>	
-	<Container className='mt-3 ml-3'>
-		<Form.Item
+			<a>
+				<span className="ml-3">
+					<small onClick={() => next(0)}>Change?</small>
+				</span>
+			</a>
+			{accStat === "I'm still searching" ? <Reject /> : ''}
+			<Box stat={accStat}>
+				<Container className="mt-3 ml-3">
+					<Form.Item
 						name={[ 'last_rent_amount' ]}
 						label="How much was your last rent?"
 						rules={[ { required: true } ]}
 					>
-						<Input addonBefore='₦' />
-					</Form.Item>
-					
-					<Form.Item
-						name={[ 'rent_collector' ]}
-						label="Who did you pay to??"
-						rules={[ { required: true } ]}
-					>
-							<Radio.Group
-					onChange={(e) => {
-						setRc(e.target.value);
-					}}
-			
-					// name="radiogroup"
-				>
-					{/* rc => rent colector */}
-					<StyledRadio type='button' className="" switch='Landlord' stat={rc}>
-						<Radio className="fg" value='Landlord'>
-							Landlord
-						</Radio>
-					</StyledRadio>
-
-					<StyledRadio type='button' className="mr-3" switch='Caretaker' stat={rc}>
-						<Radio value='Caretaker'>Caretaker</Radio>
-					</StyledRadio>
-
-					<StyledRadio type='button' switch='Agent' stat={rc}>
-						<Radio value='Agent'>Agent</Radio>
-					</StyledRadio>
-				</Radio.Group>
-
+						<Input addonBefore="₦" />
 					</Form.Item>
 
-					<Form.Item name={[ 'pay_type' ]} label="How did you pay?" rules={[ { required: true } ]} >
-				<Select size='large'  style={{width: '96%'}} placeholder='Select how you paid'>
-					
-						<Select.Option  value='card'>
-						Card
-						</Select.Option>
-						<Select.Option  value='bank'>
-						Bank Transfer
-						</Select.Option>
-					
-				</Select> 
-			</Form.Item>
-					</Container>
-	
-</Box>
+					<Form.Item name={[ 'rent_collector' ]} label="Who did you pay to??" rules={[ { required: true } ]}>
+						<Radio.Group
+							onChange={(e) => {
+								setRc(e.target.value);
+							}}
 
-				{/* <StyledSwitch className="mr-4">
+							// name="radiogroup"
+						>
+							{/* rc => rent colector */}
+
+							<StyledRadio type="button" className="" switch="Landlord" stat={rc}>
+								<Radio className="fg" value="Landlord">
+									Landlord
+								</Radio>
+							</StyledRadio>
+
+							<StyledRadio type="button" className="mr-3" switch="Caretaker" stat={rc}>
+								<Radio value="Caretaker">Caretaker</Radio>
+							</StyledRadio>
+
+							<StyledRadio type="button" switch="Agent" stat={rc}>
+								<Radio value="Agent">Agent</Radio>
+							</StyledRadio>
+						</Radio.Group>
+					</Form.Item>
+
+					<Form.Item name={[ 'pay_type' ]} label="How did you pay?" rules={[ { required: true } ]}>
+						<Select size="large" style={{ width: '96%' }} placeholder="Select how you paid">
+							<Select.Option value="card">Card</Select.Option>
+							<Select.Option value="bank">Bank Transfer</Select.Option>
+						</Select>
+					</Form.Item>
+				</Container>
+			</Box>
+
+			{/* <StyledSwitch className="mr-4">
 					<Col>
 						<strong className="switch-text">1</strong>
 					</Col>
@@ -246,15 +258,15 @@ const Form4 = ({ next,prev, payment, rent}) => {
 						<p className="switch-text">Months</p>
 					</Col>{' '}
 				</StyledSwitch> */}
-					{/* <Form.Item
+			{/* <Form.Item
 						name={[ 'employer_address' ]}
 						label="Employer or Business address"
 						rules={[ { required: true } ]}
 					>
 						<Input  />
 					</Form.Item> */}
-				
-				<BottomNav prev={prev}/>
+
+			<BottomNav prev={prev} />
 		</Form>
 	);
 };
@@ -264,4 +276,3 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps)(Form4);
-
