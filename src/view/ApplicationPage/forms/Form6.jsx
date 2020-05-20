e6ac95ac3ebaec1ff87e7cf6c1f51bb129e9ba70
kwabang01 <kwabang01@gmail.com>
2020-05-20 14:57:27 +0100

    fix form 6

--- src/view/ApplicationPage/forms/Form6.jsx
+++ src/view/ApplicationPage/forms/Form6.jsx
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Input, Space } from 'antd';
 // import CurrencyFormat from 'react-currency-format'
-import {BottomNav} from '../BottomNav'
+import { connect, useDispatch } from 'react-redux';
+import { BottomNav } from '../BottomNav';
 // import { Col, Row } from 'react-bootstrap';
 
 const layout = {
 	labelCol: { span: 16 },
 	wrapperCol: { span: 16 }
 };
-const Form6 = (props) => {
+const Form6 = ({ prev, application }) => {
 	const [ form ] = Form.useForm();
-	const { next, prev } = props;
-	
+	const dispatch = useDispatch();
+	const { referee } = application;
+
 	const validateMessages = {
 		required: 'This field is required!',
 		types: {
@@ -23,79 +25,72 @@ const Form6 = (props) => {
 		}
 	};
 
-	useEffect(() => {
-		const data = JSON.parse(localStorage.getItem('referee'));
-		if (data) {
-			form.setFieldsValue({
-				referee_firstname: data.referee_firstname,
-				referee_lastname: data.referee_lastname,
-				referee_address: data.referee_address,
-				referee_phone: data.referee_phone,
-				referee_email: data.referee_email,
-				referee_relationship: data.referee_relationship
-			});
-		}
-	}, [form]);
+	useEffect(
+		() => {
+			if (referee) {
+				form.setFieldsValue({
+					full_name: referee.full_name,
+					address: referee.address,
+					phone: referee.phone,
+					email: referee.email,
+					relationship: referee.relationship
+				});
+			}
+		},
+		[ form ]
+	);
 
 	const onFinish = (fielValues) => {
 		console.log('Received values of form: ', fielValues);
-		fielValues.dob = fielValues.dob.format();
+
 		const values = {
-			...fielValues
+			referee: { ...fielValues }
 		};
-		const data = JSON.stringify(values);
-		localStorage.setItem('personal', data);
-		
+		dispatch({
+			type: 'ADD_APPL',
+			data: values,
+			page: 5
+		});
+		console.log(application);
 	};
 
 	return (
 		<Form
 			form={form}
-			
 			{...layout}
 			name="nest-messages"
 			onFinish={onFinish}
 			validateMessages={validateMessages}
 			layout="vertical"
 		>
-		
-			<Form.Item
-				name={[ 'full_name' ]}
-				label="Full name"
-				rules={[ {  required: true } ]}
-				
-			>
-				<Input style={{ width: '58%' }} placeholder='Enter full name'/>
+			<Form.Item name={[ 'full_name' ]} label="Full name" rules={[ { required: true } ]}>
+				<Input style={{ width: '58%' }} placeholder="Enter full name" />
 			</Form.Item>
 
-			
-            <Form.Item
-				name={[ 'address' ]}
-				label="Address"
-				rules={[ {  required: true } ]}
-				
-			>
-				<Input style={{ width: '58%' }} placeholder='Enter address'/>
+			<Form.Item name={[ 'address' ]} label="Address" rules={[ { required: true } ]}>
+				<Input style={{ width: '58%' }} placeholder="Enter address" />
 			</Form.Item>
 
-
-	<Space size='small'>				<Form.Item name={[ 'phone' ]} label="Phone number" rules={[ { required: true } ]}>
-						<Input style={{ width: '120%' }} placeholder='Enter phone number'/>
-					</Form.Item>
-					
+			<Space size="small">
+				{' '}
+				<Form.Item name={[ 'phone' ]} label="Phone number" rules={[ { required: true } ]}>
+					<Input style={{ width: '120%' }} placeholder="Enter phone number" />
+				</Form.Item>
 				<Form.Item name={[ 'email' ]} label="Email address" rules={[ { required: true, type: 'email' } ]}>
-						<Input placeholder='Enter email address' style={{ width: '120%' }}/>
-						
-					</Form.Item>
-					</Space>	
-			
+					<Input placeholder="Enter email address" style={{ width: '120%' }} />
+				</Form.Item>
+			</Space>
 
 			<Form.Item name={[ 'relationship' ]} label="Relationship" rules={[ { required: true } ]}>
-						<Input placeholder='Enter relationship'  />
-						
-					</Form.Item>
-		<BottomNav prev={prev}/>
+				<Input placeholder="Enter relationship" />
+			</Form.Item>
+			<BottomNav prev={prev} />
 		</Form>
 	);
 };
-export default Form6;
+
+const mapStateToProps = (state) => ({
+	application: state.application
+});
+
+export default connect(mapStateToProps)(Form6);
